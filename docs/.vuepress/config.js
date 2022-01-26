1d753d3eac327f9b2cdb71f95531e66d073bb10c
Jay the Code Monkey <jchiarulli@pm.me>
2022-01-26 12:49:29 -0500

    Finished VuePress Tutorial 5 - Configuration Basics

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Code Monkeys',
+  description:
+    'Learn to Code like a Monkey by joining the Code Monkeys Community!!! We create videos, tutorials, blog posts, etc. to help others learn about code and other tech related topics! Start Monkeying Around Today!!! 🐵',
+  head: [
+    [
+      'meta',
+      {
+        name: 'author',
+        content: 'Jay the Code Monkey'
+      }
+    ]
+  ]
+};
