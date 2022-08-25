191920e301369f3a6a3a022039a5f9ef663a1424
Jay the Code Monkey <jchiarulli@pm.me>
2022-08-25 13:00:02 -0400

    Updating the description in the config.js file to be the same as the value currently being used in the blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'Code Monkeys',
   description:
-    'Learn to Code like a Monkey by joining the Code Monkeys Community!!! We create videos, tutorials, blog posts, etc. to help others learn about code and other tech related topics! Start Monkeying Around Today!!! 🐵',
+    'Learn to Code like a Monkey by joining the Code Monkeys Community! Check out the blog for posts and videos about coding! Start Monkeying Around Today! 🐵',
   head: [
     [
       'meta',
